9efe7bc64a3a528775fc3776d0bfb6227d3fe11d
Samad Khan <khansamad99>
2020-11-10 17:45:03 +0530

    Components Called

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import  React from 'react';
 import './App.css';
-
+import Random from './components/randomgif';
+import Tag from './components/taggif';
 function App() {
   return (
     <div className="App">
       Custom React Hooks
+      <div className="main-container">
+        <Random/>
+        <Tag/>
+      </div>
     </div>
   );
 }
