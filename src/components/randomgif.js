57733a02328a1b95ac538ea8412362c97a1c14c3
Samad Khan <khansamad99>
2020-11-11 13:01:15 +0530

    Random Tagged GIF API called

--- src/components/randomgif.js
+++ src/components/randomgif.js
@@ -2,21 +2,31 @@ import React,{useEffect,useState} from 'react'
 import axios from 'axios';
 const API_KEY = process.env.REACT_APP_API_KEY;
 const Random = () => {
-
+    const [gif, setGif] = useState('');
     //Component did mount first render
-    useEffect(() => {
-        const fetch = async () => {
-            const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const fetch = async () => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
             
-            const {data} = await axios.get(url);
-            console.log(data);
-        }
+    const {data} = await axios.get(url);
+
+    const imageSrc = data.data.images.downsized_large.url;
+        setGif(imageSrc);
+    }
+        
+
+    useEffect(() => {
         fetch();
     },[]);
 
+    const handleClick = () => {
+        fetch();
+    }
+
     return (
-        <div>
-            <h1>Random</h1>
+        <div className="container">
+            <h1>Random Gif</h1>
+            <img width="500" src={gif} alt="Random Gif" />
+            <button onClick={handleClick}>CLICK FOR NEW</button>
         </div>
     )
 }
