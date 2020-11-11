c130ad6435096ce219481e250edbcc3687404b1d
Samad Khan <khansamad99>
2020-11-11 14:19:30 +0530

    App Completed

--- src/useGif.js
+++ src/useGif.js
@@ -1,6 +1,8 @@
 import React,{useEffect,useState} from 'react'
 import axios from 'axios';
 const API_KEY = process.env.REACT_APP_API_KEY;
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
 const useGif = (tag) => {
     const [gif, setGif] = useState('');
 
